9dbab001b66be2070bf3af145ba6fb56a33795fa
Mehdi <mehdi.shakibapour@gmail.com>
2023-09-01 23:35:37 -0400

    gpt fixed index

--- routes/index.tsx
+++ routes/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <ul>
           <li><a href="/">Home</a></li>
           <li><a href="/register">Register</a></li>
-          <li><a href="./about">About</a></li>
+          <li><a href="/about">About</a></li>
         </ul>
       </nav>
     </header>
