3fd6fd734b60e789869d6fe3162f3fdfbcbd0eca
Mehdi <mehdi.shakibapour@gmail.com>
2023-09-05 13:47:32 -0400

    now fits to screen

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,43 +2,65 @@ import { tw } from "twind";
 
 export default function LoginPage() {
   return (
-    <>
-        <div className={tw`bg-gray-200 h-screen flex flex-col items-center justify-center`}>
-
-        <header className={tw`text-center`}>
-        <h1 href={"/"} className={tw`text-6xl font-bold text-blue-500`}>
-          online student directory for BMCC
-        </h1>
+    <div className={tw`flex flex-col h-screen`}>
+      <header className={tw`bg-blue-500 p-4 flex justify-between`}>
+        <a href="/" className={tw`text-white font-bold text-2xl`}>
+          Student Directory
+        </a>
       </header>
 
-<div className={tw`bg-white mt-8 p-6 rounded-lg max-w-md w-full`}>
-  <form className={tw`mt-6`}>
-    <input
-      className={tw`w-full p-2 border rounded mb-4`}
-      type="email"
-      placeholder="Email"
-    />
+      <div className={tw`flex flex-col justify-center items-center h-screen`}>
+        <div className={tw`max-w-md w-full bg-white shadow-md rounded-lg p-6`}>
+          <h1 className={tw`text-3xl font-medium text-gray-700 mb-6`}>Login</h1>
+          <form>
+            <div className={tw`mb-4`}>
+              <label className={tw`block text-gray-700 font-medium mb-2`}>
+                Email
+              </label>
+              <input
+                className={tw`w-full border rounded py-2 px-3`}
+                type="email"
+                defaultValue="@stu.bmcc.cuny.edu"
+              />
+            </div>
 
-    <input
-      className={tw`w-full p-2 border rounded mb-4`}
-      type="password"
-      placeholder="Password"
-    />
+            <div className={tw`mb-4`}>
+              <label className={tw`block text-gray-700 font-medium mb-2`}>
+                Password
+              </label>
+              <input
+                className={tw`w-full border rounded py-2 px-3`}
+                type="password"
+              />
+            </div>
 
-    <button
-      className={tw`bg-blue-500 text-white py-2 px-6 rounded-lg w-full`}
-    >
-      Log In
-    </button>
-  </form>
+            <button
+              className={tw`bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600`}
+            >
+              Sign in
+            </button>
+          </form>
+          <p className={tw`mt-4 text-center text-gray-600`}>
+            Don't have an account yet?{" "}
+            <a href={"/signup"} className={tw`text-blue-500 font-medium`}>
+              Sign up
+            </a>
+          </p>
+        </div>
+      </div>
 
-  <p className={tw`mt-4 text-center text-gray-600`}>
-    Don't have an account yet?{" "}
-    <a  href={"/signup"} className={tw`text-blue-500 font-medium`}>Sign up</a>
-  </p>
-</div>
-</div>
-    </>
+      <footer className={tw`bg-gray-200 p-4 flex justify-between`}>
+        <span>&copy; 2023 Student Directory</span>
 
+        <nav>
+          <a href="/about" className={tw`text-gray-800 mx-4`}>
+            About
+          </a>
+          <a href="/contact" className={tw`text-gray-800`}>
+            Contact
+          </a>
+        </nav>
+      </footer>
+    </div>
   );
 }
--- routes/signup.tsx
+++ routes/signup.tsx
@@ -2,38 +2,66 @@ import { tw } from "twind";
 
 export default function SignUpPage() {
   return (
-    <div
-      className={tw`bg-gray-200 h-screen flex flex-col items-center justify-center`}
-    >
-      <header className={tw`text-center`}>
-        <h1 href={"/"} className={tw`text-6xl font-bold text-blue-500`}>
-          online student directory for BMCC
-        </h1>
+    <div className={tw`flex flex-col h-screen`}>
+      <header className={tw`bg-blue-500 p-4 flex justify-between`}>
+        <a href="/" className={tw`text-white font-bold text-2xl`}>
+          Student Directory
+        </a>
       </header>
+      <div className={tw`flex flex-col justify-center items-center h-screen`}>
+      <div className={tw`max-w-md w-full bg-white shadow-md rounded-lg p-6`}>
+        <h1 className={tw`text-3xl font-medium text-gray-700 mb-6`}>Sign up</h1>
 
-      <div className={tw`bg-white mt-8 p-6 rounded-lg max-w-md w-full`}>
         <form>
-          <input
-            className={tw`w-full border p-2 rounded mb-4`}
-            placeholder="First Name"
-          />
+          <div className={tw`mb-4`}>
+            <label className={tw`block text-gray-700 font-medium mb-2`}>
+              First Name
+            </label>
+            <input
+              className={tw`w-full border rounded py-2 px-3`}
+            />
+          </div>
 
-          <input
-            className={tw`w-full border p-2 rounded mb-4`}
-            placeholder="Last Name"
-          />
-          <input
-            className={tw`w-full border p-2 rounded mb-4`}
-            placeholder="Email"
-          />
+          <div className={tw`mb-4`}>
+            <label className={tw`block text-gray-700 font-medium mb-2`}>
+              Last Name
+            </label>
+            <input
+              className={tw`w-full border rounded py-2 px-3`}
+            />
+          </div>
+
+          <div className={tw`mb-4`}>
+            <label className={tw`block text-gray-700 font-medium mb-2`}>
+              Email
+            </label>
+            <input
+              className={tw`w-full border rounded py-2 px-3`}
+              defaultValue="@stu.bmcc.cuny.edu"
+            />
+          </div>
 
           <button
-            className={tw`bg-blue-500 text-white py-2 px-4 rounded-lg w-full`}
+            className={tw`bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600`}
           >
-            Sign Up
+            Sign up
           </button>
         </form>
       </div>
+      </div>
+      <footer className={tw`bg-gray-200 p-4 flex justify-between`}>
+        <span>&copy; 2023 Student Directory</span>
+
+        <nav>
+          <a href="/about" className={tw`text-gray-800 mx-4`}>
+            About
+          </a>
+          <a href="/contact" className={tw`text-gray-800`}>
+            Contact
+          </a>
+        </nav>
+      </footer>
     </div>
+    
   );
 }
